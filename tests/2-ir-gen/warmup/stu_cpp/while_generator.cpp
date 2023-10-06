7bf02fc7200db445b732c70c109a2f0e2bd371e3
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2023-10-06 17:08:26 +0800

    Template for `*_generator.cpp`

--- tests/2-ir-gen/warmup/stu_cpp/while_generator.cpp
+++ tests/2-ir-gen/warmup/stu_cpp/while_generator.cpp
@@ -1,1 +1,32 @@
-int main() {}
+#include "BasicBlock.hpp"
+#include "Constant.hpp"
+#include "Function.hpp"
+#include "IRBuilder.hpp"
+#include "Module.hpp"
+#include "Type.hpp"
+
+#include <iostream>
+#include <memory>
+
+// 定义一个从常数值获取/创建 ConstantInt 类实例化的宏，方便多次调用
+#define CONST_INT(num) \
+    ConstantInt::get(num, module)
+
+// 定义一个从常数值获取/创建 ConstantFP 类实例化的宏，方便多次调用
+#define CONST_FP(num) \
+    ConstantFP::get(num, module)
+
+int main() {
+    // 创建一个 Module 实例
+    auto module = new Module();
+    // 创建一个 IRBuilder 实例（后续创建指令均使用此实例操作）
+    auto builder = new IRBuilder(nullptr, module);
+    // 从 Module 处取出 32 位整形 type 的实例
+    Type *Int32Type = module->get_int32_type();
+    // main()
+    auto mainFun = Function::create(FunctionType::get(Int32Type, {}), "main", module);
+    auto bb = BasicBlock::create(module, "entry", mainFun);
+    builder->set_insert_point(bb);
+    auto retAlloca = builder->create_alloca(Int32Type);
+    builder->create_store(CONST_INT(0), retAlloca); // main 函数默认 ret 0
+}
